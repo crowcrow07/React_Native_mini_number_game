6ecfde8d658e2b0b8066d0af6eb5b178480d7926
crowcrow07 <skathd3402@gmail.com>
2023-12-09 16:16:09 +0900

    feat : GameScreen 전체적인 구조 설계

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,20 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>숫자 추측</Text>
+      <View>
+        <Text>위 아래</Text>
+      </View>
+      <View></View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
