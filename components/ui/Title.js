ff4184711cb56eebcf33d7d46fd2e2b81fd54d5e
crowcrow07 <skathd3402@gmail.com>
2023-12-12 14:47:58 +0900

    feat : 폴더 구조 변경

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text } from "react-native";
 
-import { Colors } from "../constants/colors";
+import { Colors } from "../../constants/colors";
 
 export default function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
