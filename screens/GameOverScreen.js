d2f8762501b6730359408552898249239f135e31
crowcrow07 <skathd3402@gmail.com>
2023-12-13 17:13:16 +0900

    feat : 버튼 추가 및 스타일링

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,8 @@
 import { View, Image, StyleSheet, Text } from "react-native";
+
 import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
+
 import { Colors } from "../constants/colors";
 
 export default function GameOverScreen() {
@@ -12,7 +15,11 @@ export default function GameOverScreen() {
           source={require("../assets/images/success.png")}
         />
       </View>
-      <Text>당신의 휴대폰은 ?번만에 ?를 맞췄습니다.</Text>
+      <Text style={styles.summaryText}>
+        당신의 휴대폰은 <Text style={styles.highlight}>?</Text>회만에{" "}
+        <Text style={styles.highlight}>??</Text>를 맞췄습니다.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -36,5 +43,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 26,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
