94bbf7cf259657106e9eaf58f999f3b853c3d7db
crowcrow07 <skathd3402@gmail.com>
2023-12-13 17:02:01 +0900

    feat : 게임 종료 화면 스타일링

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,40 @@
-import { Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import Title from "../components/ui/Title";
+import { Colors } from "../constants/colors";
 
 export default function GameOverScreen() {
-  return <Text>Game over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>당신의 휴대폰은 ?번만에 ?를 맞췄습니다.</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
